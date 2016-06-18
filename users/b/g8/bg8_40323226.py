800791ec3e05934f7512343ba352efd281bb79c8
kmol2016spring <kmol2016spring@example.com>
2016-06-18 20:17:13 +0800

    40323226

--- users/b/g8/bg8_40323226.py
+++ users/b/g8/bg8_40323226.py
@@ -1,3 +1,9 @@
+from flask import Blueprint, request
+ 
+bg8_40323226 = Blueprint('bg8_40323226', __name__, url_prefix='/bg8_40323226', template_folder='templates')
+ 
+head_str = '''
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -19,7 +25,15 @@ brython(1);
 </script>
  
 <canvas id="plotarea" width="800" height="800"></canvas>
-
+'''
+ 
+tail_str = '''
+</script>
+</body>
+</html>
+'''
+ 
+chain_str = '''
 <script type="text/python">
 from javascript import JSConstructor
 from browser import alert
@@ -48,7 +62,11 @@ deg = math.pi/180
 # 將繪製鏈條輪廓的內容寫成 class 物件
 class chain():
     # 輪廓的外型設為 class variable
-    chamber = "M -6.8397, -1.4894             A 7, 7, 0, 1, 0, 6.8397, -1.4894             A 40, 40, 0, 0, 1, 6.8397, -18.511             A 7, 7, 0, 1, 0, -6.8397, -18.511             A 40, 40, 0, 0, 1, -6.8397, -1.4894 z"
+    chamber = "M -6.8397, -1.4894 \
+            A 7, 7, 0, 1, 0, 6.8397, -1.4894 \
+            A 40, 40, 0, 0, 1, 6.8397, -18.511 \
+            A 7, 7, 0, 1, 0, -6.8397, -18.511 \
+            A 40, 40, 0, 0, 1, -6.8397, -1.4894 z"
     #chamber = "M 0, 0 L 0, -20 z"
     cgoChamber = window.svgToCgoSVG(chamber)
  
@@ -115,82 +133,154 @@ class chain():
             cgo.render(basic1, x1, y1, self.scale, 0)
  
         return x2, y2
+'''
 
+@bg8_40323226.route('/1/<x>/<y>')
+@bg8_40323226.route('/1', defaults={'x':0, 'y':0})
+def twocircle(x,y):
+    return head_str + chain_str + oo(int(x), int(y)) + tail_str
+ 
+
+def oo(x, y):
+    '''
+從圖解法與符號式解法得到的兩條外切線座標點
+(-203.592946177111, 0.0), (0.0, 0.0), (-214.364148466539, 56.5714145924675), (-17.8936874260919, 93.9794075692901)
+(-203.592946177111, 0.0), (0.0, 0.0), (-214.364148466539, -56.5714145924675), (-17.8936874260919, -93.9794075692901)
+左邊關鍵鍊條起點 (-233.06, 49.48), 角度 20.78, 圓心 (-203.593, 0.0)
+右邊關鍵鍊條起點 (-17.89, 93.9), 角度 4.78, 圓心 (0, 0)
+    '''
+    # 20 為鏈條兩圓距
+    # chain 所圍之圓圈半徑為 20/2/math.asin(degree*math.pi/180/2)
+    # degree = math.asin(20/2/radius)*180/math.pi
+    x = 50
+    y = 0
+    degree = 20
+    first_degree = 20.78
+    startx = -233.06+100
+    starty = 49.48
+    repeat = 360 / degree
+    # 先畫出左邊第一關鍵節
+    outstring = '''
 mychain = chain()
  
-x1, y1 = mychain.basic_rot(44.0532,-165.17, 110.78)
+x1, y1 = mychain.basic_rot('''+str(startx)+","+str(starty)+", "+str(first_degree)+''')
  
-x2, y2=mychain.basic_rot(x1, y1,90.78, True) 
-x3, y3=mychain.basic_rot(x2, y2,70.78, True) 
-x4, y4=mychain.basic_rot(x3, y3,50.78, True) 
-x5, y5=mychain.basic_rot(x4, y4,30.78, True) 
-x6, y6=mychain.basic_rot(x5, y5,10.780000000000001, True) 
-x7, y7=mychain.basic_rot(x6, y6,-9.219999999999999, True) 
-x8, y8=mychain.basic_rot(x7, y7,-29.22, True) 
-x9, y9=mychain.basic_rot(x8, y8,-49.22, True) 
-x10, y10=mychain.basic_rot(x9, y9,-69.22, True) 
-x11, y11=mychain.basic_rot(x10, y10,-89.22, True) 
-x12, y12=mychain.basic_rot(x11, y11,-109.22) 
-x13, y13=mychain.basic_rot(x12, y12,-129.22) 
-x14, y14=mychain.basic_rot(x13, y13,-149.22) 
-x15, y15=mychain.basic_rot(x14, y14,-169.22) 
-x16, y16=mychain.basic_rot(x15, y15,-189.22) 
-x17, y17=mychain.basic_rot(x16, y16,-209.22) 
-x18, y18=mychain.basic_rot(x17, y17,-229.22) 
-
+'''
+    # 接著繪製左邊的非虛擬鍊條
+    for i in range(2, int(repeat)+1):
+        if i >=13 and i <=11:
+            # virautl chain
+            #outstring += "x"+str(i)+", y"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", y"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
+            outstring += "x"+str(i)+", y"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", y"+str(i-1)+","+str(first_degree+degree-i*degree)+", True) \n"
+        else:
+            outstring += "x"+str(i)+", y"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", y"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
+ 
+    # 接著處理右邊的非虛擬鍊條
+    # 先畫出右邊第一關鍵節
+ 
+    p = -17.89+100
+    k = 93.98
+    degree = 12
+    first_degree = 4.78
+    repeat = 360 / degree
+    # 第1節不是 virtual chain
+    outstring += '''
 #mychain = chain()
  
-p1, k1 = mychain.basic_rot(0,50, 94.78)
-p2, k2=mychain.basic_rot(p1, k1,82.78) 
-p3, k3=mychain.basic_rot(p2, k2,70.78) 
-p4, k4=mychain.basic_rot(p3, k3,58.78) 
-p5, k5=mychain.basic_rot(p4, k4,46.78) 
-p6, k6=mychain.basic_rot(p5, k5,34.78) 
-p7, k7=mychain.basic_rot(p6, k6,22.78) 
-p8, k8=mychain.basic_rot(p7, k7,10.780000000000001) 
-p9, k9=mychain.basic_rot(p8, k8,-1.2199999999999989) 
-p10, k10=mychain.basic_rot(p9, k9,-13.219999999999999) 
-p11, k11=mychain.basic_rot(p10, k10,-25.22) 
-p12, k12=mychain.basic_rot(p11, k11,-37.22) 
-p13, k13=mychain.basic_rot(p12, k12,-49.22) 
-p14, k14=mychain.basic_rot(p13, k13,-61.22) 
-p15, k15=mychain.basic_rot(p14, k14,-73.22) 
-p16, k16=mychain.basic_rot(p15, k15,-85.22) 
-p17, k17=mychain.basic_rot(p16, k16,-97.22) 
-p18, k18=mychain.basic_rot(p17, k17,-109.22, True) 
-p19, k19=mychain.basic_rot(p18, k18,-121.22, True) 
-p20, k20=mychain.basic_rot(p19, k19,-133.22, True) 
-p21, k21=mychain.basic_rot(p20, k20,-145.22, True) 
-p22, k22=mychain.basic_rot(p21, k21,-157.22, True) 
-p23, k23=mychain.basic_rot(p22, k22,-169.22, True) 
-p24, k24=mychain.basic_rot(p23, k23,-181.22, True) 
-p25, k25=mychain.basic_rot(p24, k24,-193.22, True) 
-p26, k26=mychain.basic_rot(p25, k25,-205.22, True) 
-p27, k27=mychain.basic_rot(p26, k26,-217.22, True) 
-p28, k28=mychain.basic_rot(p27, k27,-229.22, True) 
-p29, k29=mychain.basic_rot(p28, k28,-241.22, True) 
-p30, k30=mychain.basic_rot(p29, k29,-253.22, True) 
+p1, k1 = mychain.basic_rot('''+str(p)+","+str(k)+", "+str(first_degree)+''')
+'''
+    for i in range(2, int(repeat)+1):
+        if i >=36:
+            # virautl chain
+            outstring += "p"+str(i)+", k"+str(i)+"=mychain.basic_rot(p"+str(i-1)+", k"+str(i-1)+","+str(first_degree+degree-i*degree)+", True) \n"
+            #outstring += "p"+str(i)+", k"+str(i)+"=mychain.basic_rot(p"+str(i-1)+", k"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
+        else:
+            outstring += "p"+str(i)+", k"+str(i)+"=mychain.basic_rot(p"+str(i-1)+", k"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
+ 
 
-m1, n1 = mychain.basic_rot(x1, y1, 100.78)
-m2, n2=mychain.basic_rot(m1, n1, 100.78)
-m3, n3=mychain.basic_rot(m2, n2, 100.78)
-m4, n4=mychain.basic_rot(m3, n3, 100.78)
-m5, n5=mychain.basic_rot(m4, n4, 100.78)
-m6, n6=mychain.basic_rot(m5, n5, 100.78)
-m7, n7=mychain.basic_rot(m6, n6, 100.78)
-m8, n8=mychain.basic_rot(m7, n7, 100.78)
-m9, n9=mychain.basic_rot(m8, n8, 100.78)
-m10, n10=mychain.basic_rot(m9, n9, 100.78)
+ 
+    return outstring
+@bg8_40323226.route('/2/<x>/<y>')
+@bg8_40323226.route('/2', defaults={'x':0, 'y':0})
+def twocircle2(x,y):
+    return head_str + chain_str + oo2(int(x), int(y)) + tail_str
+ 
 
-r1, s1 = mychain.basic_rot(x11, y11, 79.22)
-r2, s2=mychain.basic_rot(r1, s1, 79.22)
-r3, s3=mychain.basic_rot(r2, s2, 79.22)
-r4, s4=mychain.basic_rot(r3, s3, 79.22)
-r5, s5=mychain.basic_rot(r4, s4, 79.22)
-r6, s6=mychain.basic_rot(r5, s5, 79.22)
-r7, s7=mychain.basic_rot(r6, s6, 79.22)
-r8, s8=mychain.basic_rot(r7, s7, 79.22)
-r9, s9=mychain.basic_rot(r8, s8, 79.22)
-r10, s10=mychain.basic_rot(r9, s9, 79.22)
-
-</script>
+def oo2(x, y):
+    '''
+從圖解法與符號式解法得到的兩條外切線座標點
+(-203.592946177111, 0.0), (0.0, 0.0), (-214.364148466539, 56.5714145924675), (-17.8936874260919, 93.9794075692901)
+(-203.592946177111, 0.0), (0.0, 0.0), (-214.364148466539, -56.5714145924675), (-17.8936874260919, -93.9794075692901)
+左邊關鍵鍊條起點 (-233.06, 49.48), 角度 20.78, 圓心 (-203.593, 0.0)
+右邊關鍵鍊條起點 (-17.89, 93.9), 角度 4.78, 圓心 (0, 0)
+    '''
+    # 20 為鏈條兩圓距
+    # chain 所圍之圓圈半徑為 20/2/math.asin(degree*math.pi/180/2)
+    # degree = math.asin(20/2/radius)*180/math.pi
+    x = 50
+    y = 0
+    degree = 20
+    first_degree = 90+20
+    startx = -56.00
+    starty = -125
+    repeat = 360 / degree
+    # 先畫出左邊第一關鍵節
+    outstring = '''
+mychain = chain()
+ 
+x1, y1 = mychain.basic_rot('''+str(startx)+","+str(starty)+", "+str(first_degree)+''')
+ 
+'''
+    # 接著繪製左邊的非虛擬鍊條
+    for i in range(2, int(repeat)+1):
+        if i >=2 and i <=11:
+            # virautl chain
+            #outstring += "x"+str(i)+", y"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", y"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
+            outstring += "x"+str(i)+", y"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", y"+str(i-1)+","+str(first_degree+degree-i*degree)+", True) \n"
+        else:
+            outstring += "x"+str(i)+", y"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", y"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
+ 
+    # 接著處理右邊的非虛擬鍊條
+    # 先畫出右邊第一關鍵節
+ 
+    p = -100
+    k = 93.98
+    degree = 12
+    first_degree = 95
+    repeat = 360 / degree
+    # 第1節不是 virtual chain
+    outstring += '''
+#mychain = chain()
+ 
+p1, k1 = mychain.basic_rot('''+str(p)+","+str(k)+", "+str(first_degree)+''')
+'''
+    for i in range(2, int(repeat)+1):
+        if i >=18:
+            # virautl chain
+            outstring += "p"+str(i)+", k"+str(i)+"=mychain.basic_rot(p"+str(i-1)+", k"+str(i-1)+","+str(first_degree+degree-i*degree)+", True) \n"
+            #outstring += "p"+str(i)+", k"+str(i)+"=mychain.basic_rot(p"+str(i-1)+", k"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
+        else:
+            outstring += "p"+str(i)+", k"+str(i)+"=mychain.basic_rot(p"+str(i-1)+", k"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
+ 
+    # 上段連接直線
+    # 從 x1, y1 作為起點
+    first_degree = 10.78+90
+    repeat = 10
+    outstring += '''
+m1, n1 = mychain.basic_rot(x1, y1, '''+str(first_degree)+''')
+'''
+    for i in range(2, int(repeat)+1):
+        outstring += "m"+str(i)+", n"+str(i)+"=mychain.basic_rot(m"+str(i-1)+", n"+str(i-1)+", "+str(first_degree)+")\n"
+ 
+    # 下段連接直線
+    # 從 x11, y11 作為起點
+    first_degree = -10.78+90
+    repeat = 10
+    outstring += '''
+r1, s1 = mychain.basic_rot(x11, y11, '''+str(first_degree)+''')
+'''
+    for i in range(2, int(repeat)+1):
+        outstring += "r"+str(i)+", s"+str(i)+"=mychain.basic_rot(r"+str(i-1)+", s"+str(i-1)+", "+str(first_degree)+")\n"
+ 
+    return outstring
+ 
