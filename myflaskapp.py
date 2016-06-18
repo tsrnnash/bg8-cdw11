e508a098c97f2a4592ca05a831338222aea6499e
nashnash <40323213@gm.nfu.edu.tw>
2016-06-18 23:10:32 +0800

    40323213

--- myflaskapp.py
+++ myflaskapp.py
@@ -7,8 +7,13 @@ import os
 import init
 
 #bg8
+import users.b.g8.bg8_40323213
+import users.b.g8.bg8_40323213_1
 import users.b.g8.bg8_40323237
 import users.b.g8.bg8_40323237_1
+import users.b.g8.bg8_40323229
+import users.b.g8.bg8_40323242
+import users.b.g8.bg8_40323226
 
 # 確定程式檔案所在目錄, 在 Windows 有最後的反斜線
 _curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -382,6 +387,12 @@ if __name__ == "__main__":
     app.run()
 
 #bg8
+app.register_blueprint(users.b.g8.bg8_40323213.bg8_40323213)
+app.register_blueprint(users.b.g8.bg8_40323213_1.bg8_40323213_1)
 app.register_blueprint(users.b.g8.bg8_40323237.bg8_40323237)
-
 app.register_blueprint(users.b.g8.bg8_40323237_1.bg8_40323237_1)
+app.register_blueprint(users.b.g8.bg8_40323229.bg8_40323229)
+app.register_blueprint(users.b.g8.bg8_40323242.bg8_40323242)
+app.register_blueprint(users.b.g8.bg8_40323226.bg8_40323226)
+
+
